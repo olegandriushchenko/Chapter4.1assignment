8d6eab99ba4ae6c16cf76861c7c85df948db99f7
oleg_andriushchenko <olegandriushchenko95@gmail.com>
2022-09-13 20:27:33 -0500

    Homework

---
+++ Chapter4.1assignment.py
@@ -0,0 +1,13 @@
+# Modify this function to return a list of strings as defined above
+
+def list_benefits():
+    return["More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"]"More organized code", "More readable code", "Easier code reuse"]
+def build_sentence(benefit):
+    return benefit "is a benefit of functions!"
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    print(build_sentence(list_of_benefits[0]))
+    print(build_sentence(list_of_benefits[1]))
+    print(build_sentence(list_of_benefits[2]))
+    print(build_sentence(list_of_benefits[3]))
+name_the_benefits_of_functions()
