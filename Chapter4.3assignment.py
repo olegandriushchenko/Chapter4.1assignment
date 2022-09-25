3929f6e7972d84204a7ee22a9c2e539bebe48430
oleg_andriushchenko <olegandriushchenko95@gmail.com>
2022-09-25 17:41:07 -0500

    Corrections

--- Chapter4.3assignment.py
+++ Chapter4.3assignment.py
@@ -1,21 +1,21 @@
 #first part of the assignment
 clothes = ["socks", "shirt", "skirt", "scarf"]
-new_cloth = ["pants", "shoes"]
-def insert_element():
+def insert_element(new_cloth = "hats", index):
     clothes.insert(index, new_cloth)
     print(clothes)
-clothes1.insert_element(0, new_cloth)
-clothes2.insert_element(-5, new_cloth)
-clothes3.insert_element(new_cloth)
+insert_element("shoes", 1)
+insert_element("coat", -2)
+insert_element("suit")
+
+insert_element()
 
 
 #secend part of the assignment
 employee_shift = ["Mike", "Andrew", "Emma", "Kelly", "John", "Brad"]
-old_employee = ["Mike", "Andrew", "Emma", "Kelly", "John", "Brad"]
 def replace_employee(employee_shift, old_employee, new_employee):
-    new_employee.insert(index, old_employee)
-    print(new_employee)
+    old_employee_index = employee_shift.index(old_employee)
+    employee_shift.remove(old_employee)
+    employee_shift.insert(old_employee_index, new_employee)
+    print(employee_shift)
 
-newEmploeyee.replace_employee(0, old_employee)
-
-employeeShift.replace_employee(3, "Maria")
+replace_employee(employee_shift, "Kelly", "Maria")
